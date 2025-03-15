06babfe88ef1d9b312ac7d2a62575d41f38da93e
theritikkk <ritikraj2004@gmail.com>
2025-03-15 20:10:09 +0530

    added details form homepage

---
+++ project_1/src/components/accordian/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id : '1',
+        question : "What are the accordian components ? ",
+        answer : " Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way.",
+    },
+
+    {
+        id : '2',
+        question : "What are they used for ? ",
+        answer : "They are used for showing and hiding the content. It is used to show the content in collapsible way. They are used for showing and hiding the content. It is used to show the content in collapsible way. They are used for showing and hiding the content. It is used to show the content in collapsible way. They are used for showing and hiding the content. It is used to show the content in collapsible way. They are used for showing and hiding the content. It is used to show the content in collapsible way. They are used for showing and hiding the content. It is used to show the content in collapsible way.",
+    },
+
+    {
+        id : '3',
+        question : "What are the types of accordian components ? ",
+        answer : "There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian. There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian. There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian. There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian. There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian. There are two types of accordian components. They are : 1. Controlled Accordian 2. Uncontrolled Accordian.",
+    },
+
+    {
+        id : '4',
+        question : "What are the props used in accordian components ? ",
+        answer : "There are two props used in accordian components. They are : 1. title 2. children. There are two props used in accordian components. They are : 1. title 2. children. There are two props used in accordian components. They are : 1. title 2. children. There are two props used in accordian components. They are : 1. title 2. children. There are two props used in accordian components. They are : 1. title 2. children. There are two props used in accordian components. They are : 1. title 2. children.",
+    },
+
+    {
+        id : '5',
+        question : "What are the uses of accordian components ? ",
+        answer : "Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way. Accordian components are used to show and hide the content. It is used to show the content in collapsible way.",
+    },
+
+];
+
+export default data;
---
+++ project_1/src/components/accordian/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    return (
+      <div className="wrapper">
+        <div className="accordian">
+          {data && data.length > 0 ? (
+            data.map((dataItem) => (
+
+              <div className="item">
+
+                <div className="title">
+                  <h3>{dataItem.question}</h3>
+                  <span>+</span>
+                </div>
+                
+              </div>
+            )
+            )
+          ) : (
+            <div>No data found !</div>
+          )}
+        </div>
+      </div>
+    );
+    
+}
