1c0b97d74b8cdee17a6cb3b5a9c18c9435bcf2d7
theritikkk <ritikraj2004@gmail.com>
2025-03-23 22:36:52 +0530

    made a new project of color generation

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      
+      {/* <Accordian /> */}
+
+      <RandomColor/>
     </div>
   );
 }
