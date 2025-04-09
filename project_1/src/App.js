5b1f0321f380e1608f9960ea9e3bfd989f3709d7
theritikkk <ritikraj2004@gmail.com>
2025-04-10 04:52:44 +0530

    made the amends

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,21 +1,23 @@
-
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
+      {/* <Accordian />
 
-      {/* <Accordian /> */}
+      <RandomColor/>
 
-      {/* <RandomColor/> */}
+      <StarRating noOfStar={10}/> */}
 
-      {/* <StarRating noOfStar={10}/> */}
-
-      
-
+      <ImageSlider
+        url={'https://picsum.photos/v2/list'}
+        page={"1"}
+        limit={"5"}
+      />
     </div>
   );
 }
