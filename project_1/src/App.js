7e9617e9cc335a72784daa349ac3b2372d5f1e14
theritikkk <ritikraj2004@gmail.com>
2025-04-14 18:47:00 +0530

    made custom-tabs feature

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -9,6 +9,7 @@ import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
 import ScrollIndicator from "./components/scroll-indicator";
+import TabsTest from "./components/custom-tabs/tab-test";
 
 function App() {
   return (
@@ -39,7 +40,7 @@ function App() {
 
       {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
 
-      
+      < TabsTest />
 
       
     </div>
