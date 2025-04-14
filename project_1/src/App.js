6278a9420345040172ff76ad5fccb9dfa118d4fb
theritikkk <ritikraj2004@gmail.com>
2025-04-14 17:45:29 +0530

    made the scroll percentage

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -8,6 +8,7 @@ import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   return (
@@ -34,7 +35,11 @@ function App() {
       {/* <QRCodeGenerator /> */}
 
 
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
+
+      
 
       
     </div>
