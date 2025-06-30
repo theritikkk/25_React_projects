7cfcecee4e0cc34c6f5f03c7f2a7a3e86fc8bea3
theritikkk <ritikraj2004@gmail.com>
2025-06-30 18:47:32 +0530

    made ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,86 +1,94 @@
 import "./App.css";
 import Accordian from "./components/accordian";
-import ImageSlider from "./components/image-slider";
-import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
-import LoadMoreData from "./components/load-more-data"
-import TreeView from "./components/tree-view";
-import menus from "./components/tree-view/data";
-import QRCodeGenerator from "./components/qr-code-generator";
-import LightDarkMode from "./components/light-dark-mode";
-import ScrollIndicator from "./components/scroll-indicator";
-import TabsTest from "./components/custom-tabs/tab-test";
 import ModalTest from "./components/custom-model-popup/modal-test";
-import GithubProfileFinder from "./components/github-profile-finder";
-import SearchAutoComplete from "./components/search-autocomplete-with-api";
+import TabTest from "./components/custom-tabs/tab-test";
 import FeatureFlags from "./components/feature-flag";
-import FeatureFlagsGlobalState from "./components/feature-flag/context";
-import UseFetchHookTest from "./components/use-fetch/test";
-import UseOnClickOutsideTest from "./components/use-outside-click/test";
-import UseWindowResizeTest from "./components/use-window-resize/test";
-import ScrollToTopAndToBottom from "./components/scroll-to-top-and-bottom/index";
+import FeatureFlagGlobalState from "./components/feature-flag/context";
+import GithubProfileFinder from "./components/github-profile-finder";
+import ImageSlider from "./components/image-slider";
+import LightDarkMode from "./components/light-dark-mode";
+import LoadMoreData from "./components/load-more-data";
+import QRCodeGenerator from "./components/qr-code-generator";
+import RandomColor from "./components/random-color";
+import ScrollIndicator from "./components/scroll-indicator";
+import ScrollToTopAndBottom from "./components/scroll-to-top-and-bottom";
 import ScrollToSection from "./components/scroll-to-top-and-bottom/scroll-to-section";
+import SearchAutocomplete from "./components/search-autocomplete-with-api";
+import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data.js";
+import UseFetchHookTest from "./components/use-fetch/test";
+import UseOnclickOutsideTest from "./components/use-outside-click/test";
+import UseWindowResizeTest from "./components/use-window-resize/test.jsx";
 
 function App() {
-
   return (
-
     <div className="App">
+      
+      {/* Accordian component */}
+      <Accordian/>
 
-      <Accordian />
+      {/* Random color component */}
+      <RandomColor/>
 
-      RandomColor/>
+      {/* Star rating component */}
+      <StarRating noOfStars={10}/>
 
-      <StarRating noOfStar={10}/>
-
+      {/* Image slider component */}
       <ImageSlider
-        url={'https://picsum.photos/v2/list'}
+        url={"https://picsum.photos/v2/list"}
         page={"1"}
-        limit={"5"}
+        limit={"10"}
+      />
 
-      
-      
-      <LoadMoreData />
+      {/* Load more products component */}
+      <LoadMoreData/>
 
+      {/* Tree view component/menu UI component / recursive navigation menu */}
       <TreeView menus={menus} />
 
+      {/* QR code generator */}
+      <QRCodeGenerator/>
 
-      <QRCodeGenerator />
+      {/* light and dark theme switch */}
+      <LightDarkMode/>
 
+      {/* Scroll indicator component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
 
-      <LightDarkMode />
+      {/* Custom tabs component */}
+      <TabTest />
 
-      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
+      {/* Custom Modal Component */}
+      <ModalTest/>
 
-       < TabsTest />
+      {/* Github profile finder */}
+      <GithubProfileFinder/>
 
-      <ModalTest />
+      {/* Search Autocomplete */}
+      <SearchAutocomplete/>
 
-       <GithubProfileFinder /> 
-
-       <SearchAutoComplete />
-      
-      <FeatureFlagsGlobalState>
+      {/* Feature Flag IMplementation */}
+      <FeatureFlagGlobalState>
         <FeatureFlags />
-      </FeatureFlagsGlobalState>
+      </FeatureFlagGlobalState>
 
-      <UseFetchHookTest />
+      {/* useFetch - Custom Hook Test*/}
+      <UseFetchHookTest/>
 
-      <UseOnClickOutsideTest />
+      {/* Use Onclick Outside Hook Test */}
+      <UseOnclickOutsideTest/>
 
-      <UseWindowResizeTest />
+      {/* Use Window Resize Hook Test */}
+      <UseWindowResizeTest/>
 
-      <ScrollToTopAndToBottom />
+      {/* Scroll to Top and Bottom */}
+      <ScrollToTopAndBottom/>
 
-      <ScrollToSection />
-
-      
-      
-
+      {/* Scroll to a Particular Section */}
+      <ScrollToSection/>
     </div>
-
   );
-  
 }
 
 export default App;
